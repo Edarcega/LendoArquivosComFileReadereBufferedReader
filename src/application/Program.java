ea96bfe6084338b6ff17f0ec46b3cb76dcfa6960
Edarcega <edimar.eds@gmail.com>
2021-11-16 21:13:16 -0300

    Adicionado bloco Try with resources para reduzir o codigo

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,13 +7,8 @@ import java.io.IOException;
 public class Program {
     public static void main(String[] args) {
         String path = "/home/edimar/Documentos/Teste";
-        BufferedReader br = null;
-        FileReader fr = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
             String line = br.readLine();
 
             while (line != null) {
@@ -22,15 +17,6 @@ public class Program {
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            try {
-                if (br != null)
-                    br.close();
-                if (fr != null)
-                    fr.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
